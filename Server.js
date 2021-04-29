c15221b43af991793aa392c0b11910e379aa2ae2
Ashtyn Ce <ashtyncza@gmail.com>
2021-04-28 23:06:58 -0400

    melting brain

--- Server.js
+++ Server.js
@@ -34,30 +34,64 @@ app.get('/posts', paginatedResults(posts), (req, res) => {
 })
 
 app.get('/users', (req, res) => {
-    const page = parseInt(req.query.page)
-    const limit = parseInt(req.query.limit)
+    // const page = parseInt(req.query.page)
+    // const limit = parseInt(req.query.limit)
 
-    const startIndex = (page - 1) * limit
-    const endIndex = page * limit
+    // const startIndex = (page - 1) * limit
+    // const endIndex = page * limit
 
-    const results = {}
+    // const results = {}
 
-    if (endIndex < users.length) {
-        results.next = {
-            page: page + 1,
-            limit: limit
-        }
-    }
+    // if (endIndex < users.length) {
+    //     results.next = {
+    //         page: page + 1,
+    //         limit: limit
+    //     }
+    // }
 
-    if (startIndex > 0) {
-        results.previous = {
-            page: page - 1,
-            limit: limit
-        }
-    }
+    // if (startIndex > 0) {
+    //     results.previous = {
+    //         page: page - 1,
+    //         limit: limit
+    //     }
+    // }
 
-    results.results = users.slice(startIndex, endIndex)
-    res.json(results)
+    // results.results = users.slice(startIndex, endIndex)
+    // res.json(results)
+
+    res.json(res.paginatedResults)
 })
+
+function paginatedResults(model) {
+    return (req, res, next) => {
+        const page = parseInt(req.query.page)
+        const limit = parseInt(req.query.limit)
+
+        const startIndex = (page - 1) * limit
+        const endIndex = page * limit
+
+        const results = {}
+
+        if (endIndex < model.length) {
+            results.next = {
+                page: page + 1,
+                limit: limit
+            }
+        }
+
+        if (startIndex > 0) {
+            results.previous = {
+                page: page - 1,
+                limit: limit
+            }
+        }
+
+        results.results = model.slice(startIndex, endIndex)
+
+        res.paginatedResults = results
+        next()
+
+    }
+}
 
 app.listen(4000)
