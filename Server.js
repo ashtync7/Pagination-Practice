d246323a180fbb1cca7119aab253d433df0e68b7
Ashtyn Ce <ashtyncza@gmail.com>
2021-04-28 22:54:26 -0400

    oh Lawd

--- Server.js
+++ Server.js
@@ -16,23 +16,44 @@ const users = [
     { id: 10, name: 'User 10' }
 ]
 
+const posts = [
+    { id: 1, name: 'Post 1' },
+    { id: 2, name: 'Post 2' },
+    { id: 3, name: 'Post 3' },
+    { id: 4, name: 'Post 4' },
+    { id: 5, name: 'Post 5' },
+    { id: 6, name: 'Post 6' },
+    { id: 7, name: 'Post 7' },
+    { id: 8, name: 'Post 8' },
+    { id: 9, name: 'Post 9' },
+    { id: 10, name: 'Post 10' }
+]
+
+app.get('/posts', paginatedResults(posts), (req, res) => {
+
+})
+
 app.get('/users', (req, res) => {
-    const page = req.query.page
-    const limit = req.query.limit
+    const page = parseInt(req.query.page)
+    const limit = parseInt(req.query.limit)
 
     const startIndex = (page - 1) * limit
     const endIndex = page * limit
 
     const results = {}
 
-    results.next = {
-        page: page + 1,
-        limit: limit
+    if (endIndex < users.length) {
+        results.next = {
+            page: page + 1,
+            limit: limit
+        }
     }
 
-    results.previous = {
-        page: page - 1,
-        limit: limit
+    if (startIndex > 0) {
+        results.previous = {
+            page: page - 1,
+            limit: limit
+        }
     }
 
     results.results = users.slice(startIndex, endIndex)
