ea6a47c537eaf74dd449476149f792e696b06f62
Ashtyn Ce <ashtyncza@gmail.com>
2021-05-02 23:35:33 -0400

    hm

--- Server.js
+++ Server.js
@@ -22,35 +22,35 @@ db.once('open', async () => {
     ]).then(() => console.log('Added Users'))
 })
 
-const users = [
-    { id: 1, name: 'User 1' },
-    { id: 2, name: 'User 2' },
-    { id: 3, name: 'User 3' },
-    { id: 4, name: 'User 4' },
-    { id: 5, name: 'User 5' },
-    { id: 6, name: 'User 6' },
-    { id: 7, name: 'User 7' },
-    { id: 8, name: 'User 8' },
-    { id: 9, name: 'User 9' },
-    { id: 10, name: 'User 10' }
-]
+// const users = [
+//     { id: 1, name: 'User 1' },
+//     { id: 2, name: 'User 2' },
+//     { id: 3, name: 'User 3' },
+//     { id: 4, name: 'User 4' },
+//     { id: 5, name: 'User 5' },
+//     { id: 6, name: 'User 6' },
+//     { id: 7, name: 'User 7' },
+//     { id: 8, name: 'User 8' },
+//     { id: 9, name: 'User 9' },
+//     { id: 10, name: 'User 10' }
+// ]
 
-const posts = [
-    { id: 1, name: 'Post 1' },
-    { id: 2, name: 'Post 2' },
-    { id: 3, name: 'Post 3' },
-    { id: 4, name: 'Post 4' },
-    { id: 5, name: 'Post 5' },
-    { id: 6, name: 'Post 6' },
-    { id: 7, name: 'Post 7' },
-    { id: 8, name: 'Post 8' },
-    { id: 9, name: 'Post 9' },
-    { id: 10, name: 'Post 10' }
-]
+// const posts = [
+//     { id: 1, name: 'Post 1' },
+//     { id: 2, name: 'Post 2' },
+//     { id: 3, name: 'Post 3' },
+//     { id: 4, name: 'Post 4' },
+//     { id: 5, name: 'Post 5' },
+//     { id: 6, name: 'Post 6' },
+//     { id: 7, name: 'Post 7' },
+//     { id: 8, name: 'Post 8' },
+//     { id: 9, name: 'Post 9' },
+//     { id: 10, name: 'Post 10' }
+// ]
 
-app.get('/posts', paginatedResults(posts), (req, res) => {
+// app.get('/posts', paginatedResults(posts), (req, res) => {
 
-})
+// })
 
 app.get('/users', paginatedResults(User), (req, res) => {
     // const page = parseInt(req.query.page)
@@ -112,8 +112,8 @@ function paginatedResults(model) {
             res.status(500).json({ message: e.message })
         }
 
-        res.paginatedResults = results
-        next()
+        // res.paginatedResults = results
+        // next()
 
     }
 }
