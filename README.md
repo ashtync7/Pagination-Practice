7cc285f49be463cbea5fbd166626458e131acc2f
Ashtyn Ce <ashtyncza@gmail.com>
2021-05-04 16:06:10 -0400

    final

--- README.md
+++ README.md
@@ -9,8 +9,7 @@ Prerequisites:
     -- Add seed data to populate and test 
     -- Set up schema in Users.js 
 
-2. Implemented a middleware function (paginatedResults) that will paginate all endpoints in multiples of 5
+2. Implemented a middleware function that will paginate all endpoints in multiples of 5
     -- Established parameters, page (what page the data is showm on) and limit (how many datapoints are shown on a page)
-    -- If statements for "next" (to show the next set of data) or "previous" page (to show the previous set of data)
 
 3. Connected paginated API to MonogoDB for database storage 
--- Server.js
+++ Server.js
@@ -6,7 +6,7 @@ const app = express()
 mongoose.connect('mongodb://localhost/pagination', { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.once('open', async () => {
-    if (await User.find() > 0) return
+    if (await User.countDocuments().exec() > 0) return
 
     Promise.all([
         User.create({ range: { by: { id: 1, name: "my-app-001" } } }),
@@ -27,101 +27,14 @@ db.once('open', async () => {
     ]).then(() => console.log('Added Users'))
 })
 
-// const users = [
-//     { id: 1, name: 'User 1' },
-//     { id: 2, name: 'User 2' },
-//     { id: 3, name: 'User 3' },
-//     { id: 4, name: 'User 4' },
-//     { id: 5, name: 'User 5' },
-//     { id: 6, name: 'User 6' },
-//     { id: 7, name: 'User 7' },
-//     { id: 8, name: 'User 8' },
-//     { id: 9, name: 'User 9' },
-//     { id: 10, name: 'User 10' }
-// ]
 
-// const posts = [
-//     { id: 1, name: 'Post 1' },
-//     { id: 2, name: 'Post 2' },
-//     { id: 3, name: 'Post 3' },
-//     { id: 4, name: 'Post 4' },
-//     { id: 5, name: 'Post 5' },
-//     { id: 6, name: 'Post 6' },
-//     { id: 7, name: 'Post 7' },
-//     { id: 8, name: 'Post 8' },
-//     { id: 9, name: 'Post 9' },
-//     { id: 10, name: 'Post 10' }
-// ]
+app.get('/apps', async (req, res) => {
+    const page = parseInt(req.query.page)
+    const limit = parseInt(req.query.limit)
+    console.log(page, limit, "blah")
 
-// app.get('/posts', paginatedResults(posts), (req, res) => {
-
-// })
-
-// middleware function VVVV
-app.get('/apps', paginatedResults(User), (req, res) => {
-    // const page = parseInt(req.query.page)
-    // const limit = parseInt(req.query.limit)
-
-    // const startIndex = (page - 1) * limit
-    // const endIndex = page * limit
-
-    // const results = {}
-
-    // if (endIndex < users.length) {
-    //     results.next = {
-    //         page: page + 1,
-    //         limit: limit
-    //     }
-    // }
-
-    // if (startIndex > 0) {
-    //     results.previous = {
-    //         page: page - 1,
-    //         limit: limit
-    //     }
-    // }
-
-    // results.results = users.slice(startIndex, endIndex)
-    // res.json(results)
-
-    res.json(res.paginatedResults)
+    let result = await User.find().sort({ "range.by.id": 1 }).skip((page - 1) * limit).limit(limit)
+    res.json(result)
 })
-
-function paginatedResults(model) {
-    return async (req, res, next) => {
-        const page = parseInt(req.query.page)
-        const limit = parseInt(req.query.limit)
-
-        const startIndex = (page - 1) * limit
-        const endIndex = page * limit
-
-        const results = {}
-
-        if (endIndex < await model.find()) {
-            results.next = {
-                page: page + 1,
-                limit: limit
-            }
-        }
-
-        if (startIndex > 0) {
-            results.previous = {
-                page: page - 1,
-                limit: limit
-            }
-        }
-        try {
-            res.paginatedResults = results
-            results.results = await model.find().limit(limit).skip(startIndex).exec()
-            // results.results.sort((a, b) => a.toString().localeCompare(b))
-
-
-            next()
-
-        } catch (e) {
-            res.status(500).json({ message: e.message })
-        }
-    }
-}
 
 app.listen(4000)
